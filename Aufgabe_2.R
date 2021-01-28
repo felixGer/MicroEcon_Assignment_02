40f20f1a2c4cf855d95ce894bad1d141590fc55f
felixGer <felixgermaine1@gmail.com>
2021-01-28 19:08:29 +0100

    Update Aufgabe_2.R

--- Aufgabe_2.R
+++ Aufgabe_2.R
@@ -5,22 +5,26 @@ library("stats")
 library("tidyverse")
 library("haven")
 library("xtable")
-df = as.data.frame(read_dta(file = "titanic.dta"))
+
 library(dplyr)
 library(sjmisc)
 
+#Preproc data:
+df = as.data.frame(read_dta(file = "titanic.dta"))
 #create dummy for class factor
 df %<>% 
   to_dummy(class, suffix = "label") %>% 
   bind_cols(df)
-
 colnames(df) = c("class1", "class2", "class3", "classcrew", "class", "ageadult", "man", "survived")
-#set variable types:
+
 #########################################################################################################
 ### a. Estimate probit
 Probit_Model_Estimate = glm(survived ~ class1 + class2 + class3 + ageadult  + man, family = binomial(link = "probit"), data = df)
 summary(Probit_Model_Estimate )
+
+#output latex table
 xtable(summary(Probit_Model_Estimate ))
+
 #########################################################################################################
 ### b. Marginal Effect (with formula from Lecture)
 
@@ -29,13 +33,13 @@ ME = function(x){dnorm(x) * Beta_Man }
 curve(ME(x), from=-5, to=5, , xlab="xiB", ylab="Marginal-Effect of Man", xaxp  = c(-4, 4, 16))
 
 
-#########################################################################################################
-### b.iii ' Effect of gender (not marginal)
-
+### iii ' Effect of gender (not marginal)
 ME = function(x){pnorm(x + Beta_Man) - pnorm(x) }
 curve(ME(x), from=-5, to=5, , xlab="z", ylab="Marginal-Effect of Man", xaxp  = c(-4, 4, 16))
-#Compute the maximum marginal effect for out of the set of discrete values
-covariate_matrix_all_possible_values = matrix(c(1,1,0,0,1, 1,0,1,0,1, 1, 0,0,1,1, 1, 0,0,0,1, 1, 1,0,0,0,1,  0,1,0,0, 1, 0,0,1,0,1, 0,0,0,0), byrow = TRUE, nrow = 8, ncol = 5)
+
+
+#########################################################################################################
+### c --> Already estimated in a.
 
 #########################################################################################################
 ### d.test Hypothesis
@@ -43,35 +47,18 @@ covariate_matrix_all_possible_values = matrix(c(1,1,0,0,1, 1,0,1,0,1, 1, 0,0,1,1
 #perform regression with interaction terms.:
 Probit_Model_Estimate_Interact = glm(survived ~ class1 + class2 + class3 + ageadult  + man + class1*man + class2*man + class3*man, family = binomial(link = "probit"), data = df)
 summary(Probit_Model_Estimate_Interact )
+#output latex table
 xtable(summary(Probit_Model_Estimate_Interact ))
 
 #get log likelihoods from restricted and unrestriced models:
+
+# log likelihood from model in b (restricted)
 Log_Likelihood_Restricted = logLik(Probit_Model_Estimate)
+# log likelihood from model in d (unrestricted)
 Log_Likelihood_Unrestricted = logLik(Probit_Model_Estimate_Interact)
 
 #compute test-statistics
 Test_stat = -2*(Log_Likelihood_Restricted -  Log_Likelihood_Unrestricted)
 
-#compute p_value:
+#compute p_value (under H0 the test stat is asymptotically Chi squared distributed with degrees of freedom = number of restrictions)
 p_value = 1- pchisq(Test_stat[1], df = 3)
-
-
-
-
-
-
-
-#####
-#OLD
-
-#create covariate matrix for computing xiB (with column for 1 constant)
-covariates_of_interest = df[, c("class1", "class2", "class3", "ageadult", "sexmale")]
-#add col of 1 's
-covariates_of_interest = cbind(rep(1, nrow(covariates_of_interest)), covariates_of_interest)
-#transform to matrix
-covariates_of_interest = as.matrix(covariates_of_interest)
-
-#compute the vecto
-xiB_vector =  t(Probit_Model_Estimate[["coefficients"]] %*% t(covariates_of_interest ))
-
-pnorm(xiB_vector) * Probit_Model_Estimate[["coefficients"]]["sexmale"]
