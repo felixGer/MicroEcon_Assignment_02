ec844c2a215d5f0f748ca9e1162b297d14ce6ddc
felixGer <felixgermaine1@gmail.com>
2021-01-28 18:57:46 +0100

    Update Aufgabe_2.R

--- Aufgabe_2.R
+++ Aufgabe_2.R
@@ -16,33 +16,53 @@ df %<>%
   to_dummy(class, suffix = "label") %>% 
   bind_cols(df)
 
-colnames(df) = c("class1", "class2", "class3", "classcrew", "class", "ageadult", "sexmale", "survived")
+colnames(df) = c("class1", "class2", "class3", "classcrew", "class", "ageadult", "man", "survived")
 #set variable types:
 #########################################################################################################
 ### a. Estimate probit
-Probit_Model_Estimate = glm(survived ~ class1 + class2 + class3 + ageadult  + sexmale, family = binomial(link = "probit"), data = df)
+Probit_Model_Estimate = glm(survived ~ class1 + class2 + class3 + ageadult  + man, family = binomial(link = "probit"), data = df)
+summary(Probit_Model_Estimate )
+xtable(summary(Probit_Model_Estimate ))
+#########################################################################################################
+### b. Marginal Effect (with formula from Lecture)
+
+Beta_Man = Probit_Model_Estimate[["coefficients"]]["man"]
+ME = function(x){dnorm(x) * Beta_Man }
+curve(ME(x), from=-5, to=5, , xlab="xiB", ylab="Marginal-Effect of Man", xaxp  = c(-4, 4, 16))
 
 
 #########################################################################################################
-### b. Marginal Effect
+### b.iii ' Effect of gender (not marginal)
 
-Beta_Sex = Probit_Model_Estimate[["coefficients"]]["sexmale"]
-ME = function(x){pnorm(x + Beta_Sex) - pnorm(x) }
-curve(ME(x), from=-5, to=5, , xlab="z", ylab="Marginal-Effect of Male", xaxp  = c(-4, 4, 16))
-
-## ii. Compute the maximum marginal effect for out of the set of discrete values
-
+ME = function(x){pnorm(x + Beta_Man) - pnorm(x) }
+curve(ME(x), from=-5, to=5, , xlab="z", ylab="Marginal-Effect of Man", xaxp  = c(-4, 4, 16))
+#Compute the maximum marginal effect for out of the set of discrete values
 covariate_matrix_all_possible_values = matrix(c(1,1,0,0,1, 1,0,1,0,1, 1, 0,0,1,1, 1, 0,0,0,1, 1, 1,0,0,0,1,  0,1,0,0, 1, 0,0,1,0,1, 0,0,0,0), byrow = TRUE, nrow = 8, ncol = 5)
 
-#compute xiB for each value:
-xiB_all_possible_values = Probit_Model_Estimate[["coefficients"]][1:5] %*% t(covariate_matrix_all_possible_values)
+#########################################################################################################
+### d.test Hypothesis
 
-#compute Marginal effect for each value:
-ME_all_possible_values = ME(xiB_all_possible_values)
+#perform regression with interaction terms.:
+Probit_Model_Estimate_Interact = glm(survived ~ class1 + class2 + class3 + ageadult  + man + class1*man + class2*man + class3*man, family = binomial(link = "probit"), data = df)
+summary(Probit_Model_Estimate_Interact )
+xtable(summary(Probit_Model_Estimate_Interact ))
 
-cbind(covariate_matrix_all_possible_values, t(xiB_all_possible_values), t(ME_all_possible_values))
+#get log likelihoods from restricted and unrestriced models:
+Log_Likelihood_Restricted = logLik(Probit_Model_Estimate)
+Log_Likelihood_Unrestricted = logLik(Probit_Model_Estimate_Interact)
 
-xtable(cbind(covariate_matrix_all_possible_values, t(xiB_all_possible_values), t(ME_all_possible_values)))
+#compute test-statistics
+Test_stat = -2*(Log_Likelihood_Restricted -  Log_Likelihood_Unrestricted)
+
+#compute p_value:
+p_value = 1- pchisq(Test_stat[1], df = 3)
+
+
+
+
+
+
+
 #####
 #OLD
 
